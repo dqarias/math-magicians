0eddab5a7e401f8619a78ca05d6b32dc15ee6cc7
dqarias <dino.quispea@gmail.com>
2022-07-01 17:31:47 -0500

    Refactor Calculator.js file using hooks and function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,76 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable class-methods-use-this */
+import React, { useState } from 'react';
+import Button from './Button';
+import './Calculator.css';
+import calculate from '../logic/calculate';
 
-import React from 'react';
+const Calculator = () => {
+  const numbers = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'];
+  const operColum = ['÷', 'x', '-', '+', '='];
+  const operRow = ['AC', '+/-', '%'];
+
+  const [calculateObj, setCalculateObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const { total, next } = calculateObj;
+
+  const handleClick = (buttonName) => {
+    console.log('clicked');
+    setCalculateObj((prevState) => calculate(prevState, buttonName));
+  };
+
+  const buttonNumber = () => {
+    const btnNumber = numbers.map((num) => (
+      <Button
+        key={num}
+        buttonClass={`btn ${num === '0' ? 'btn__number0' : ''}`}
+        item={num}
+        handleClick={handleClick}
+      />
+    ));
+    return btnNumber;
+  };
+
+  const buttonOperation = (operation) => {
+    const btnSignOperation = operation.map((sign) => (
+      <Button
+        key={sign}
+        buttonClass={`btn ${sign === 'AC' || sign === '+/-' || sign === '%' ? '' : 'btn__signs'}`}
+        item={sign}
+        handleClick={handleClick}
+      />
+    ));
+    return btnSignOperation;
+  };
+
+  return (
+
+    <div className="calculator">
+      <div className="calculator__container">
+        <div className="result">
+          {next || total || '0'}
+        </div>
+        <div className="operation__column">
+          {buttonOperation(operColum)}
+        </div>
+        <div className="operation__row">
+          {buttonOperation(operRow)}
+        </div>
+        <div className="numbers">
+          {buttonNumber()}
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Calculator;
+
+/* import React from 'react';
 import Button from './Button';
 import './Calculator.css';
 import calculate from '../logic/calculate';
@@ -83,4 +152,4 @@ class Calculator extends React.Component {
   }
 }
 
-export default Calculator;
+export default Calculator; */
